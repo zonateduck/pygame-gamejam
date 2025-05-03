fba2fc8ee01a37420634fa9f93376e339f7e9a54
Vibeke Ihlen Selnes <107354551+vibekeke@users.noreply.github.com>
2025-05-03 15:06:25 +0200

    Early saturday commit
    ????

--- src/game.py
+++ src/game.py
@@ -5,12 +5,31 @@ import sys, random, time
 from options import *
 from assets import *
 
+
+#Importing levels
+from Levels.TestScreen1 import TestScreen1
+from Levels.TestScreen2 import TestScreen2
+from Levels.StartScreen import StartScreen
+
+#Importing objects
+from Objects.TestObject1 import TestObject1
+from colorGrading import *
+
+from player import Player
+
+
 # Initialize pygame
 pygame.init()
 
+
 # Set up the window
-WIDTH, HEIGHT = 800, 600
+WIDTH, HEIGHT = 1920, 1080
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
+
+GAME_BACKGROUND = BACKGROUND
+background = pygame.transform.scale(pygame.image.load(GAME_BACKGROUND).convert(), (WIDTH, HEIGHT))
+
+
 # TODO: make up a name for our game
 pygame.display.set_caption("Our game name")
 
@@ -22,10 +41,74 @@ FRAMERATE = 60
 WHITE = (255, 255, 255)
 BLUE = (0, 0, 255)
 
+    
+
+
+
 # Game variables
 x, y = 100, 100
 speed = 5
 size = 50
+
+player = Player(screen, x, y, speed)
+
+obj_range = 15   #How big a range the interaction-area has
+
+
+objects = {
+    #"testobject01" : TestObject1()
+}
+
+#SUGGESTION: Keep a list of objects in the world for the sake of interaction. Update when states change
+world_objects = [
+    #[object, type, position_x, position_y]
+]
+
+world_borders = []  #Updates based on current_state.get_borders()
+transition_threshold = 5    #How many pixels off-screen before transition
+
+#GameState manager
+test_start_screen = StartScreen()
+test_screen1 = TestScreen1()
+test_screen2 = TestScreen2()
+
+current_state = test_start_screen
+
+#This makes sure the game is not in two states at the same time :)
+def change_gamestate(new_state):
+    global current_state
+    global world_borders
+
+    if current_state != new_state:
+        world_objects.clear()
+        world_borders.clear()
+        current_state = new_state
+        
+        #Update borders and objects:
+        world_borders = current_state.get_borders()
+        for objectID in current_state.get_objects():
+            if objectID in objects.keys():
+                world_objects.append(objects[objectID])
+
+#Finds correct area to move to :)
+def find_adjacent_area(current_state, direction): 
+    areaID = current_state.get_adjacent_area(direction)
+
+    match areaID:
+        case "test01" : 
+            new_state = test_screen1
+        case "test02" : 
+            new_state = test_screen2
+        case _: #If areaID did not match
+            print("WARNING: Area not found. Create collision in the .get_borders method, or attatch correct area")
+            new_state = current_state
+    return new_state
+
+def draw_world_objects():
+    pass
+
+
+
 
 # Game loop
 running = True
@@ -36,21 +119,94 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+ 
 
+    TARGET_COLOR = 		(204,0,0)
+    REPLACEMENT_COLOR = (23, 23 ,23)
     # Get key presses
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_LEFT]:
-        x -= speed
-    if keys[pygame.K_RIGHT]:
-        x += speed
-    if keys[pygame.K_UP]:
-        y -= speed
-    if keys[pygame.K_DOWN]:
-        y += speed
 
-    # Draw
-    screen.fill(WHITE)
-    pygame.draw.rect(screen, BLUE, (x, y, size, size))
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_a]:
+        if not (x < 0 and "LEFT" in world_borders):
+            #background = convert_to_black_and_white(background.copy())
+            player.x -= speed
+            player.facing = "left"
+    if keys[pygame.K_d]:
+        if not (x > (WIDTH - size) and "RIGHT" in world_borders):
+            #background = remove_blue_channel(background.copy())
+            player.x += speed
+            player.facing = "right"
+    if keys[pygame.K_w]:
+        if not (y < 0 and "UP" in world_borders):
+            #background = remove_red_channel(background.copy())
+            player.y -= speed
+            player.facing = "up"
+    if keys[pygame.K_s]:
+        if not (y > (HEIGHT - size) and "DOWN" in world_borders):
+            #background = remove_green_channel(background.copy())
+            player.y += speed
+            player.facing = "down"
+    if keys[pygame.K_1]: #DEBUG BUTTON
+        background = remove_color(background.copy(), TARGET_COLOR, (160, 160, 160))
+
+
+    #Draw background
+    screen.blit(background, (0, 0))
+
+    #Run the correct gamestate
+    #TODO replace test-screens with actual levels / screens
+    while current_state == test_start_screen:
+        #This doesnt have to be a while loop, but prevents game from moving on :)
+        test_start_screen.run(screen)
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    print("pressed space")
+                    change_gamestate(test_screen1)
+
+    if current_state == test_screen1:
+        current_state.run(screen) #This makes the current_scene display and do its stuff!
+    if current_state == test_screen2:
+        current_state.run(screen)
+
+    #Logic for player going to new area :D
+    if x < 0 - transition_threshold:
+        change_gamestate(find_adjacent_area(current_state, "LEFT"))
+        x = WIDTH
+    if x > WIDTH + transition_threshold:
+        change_gamestate(find_adjacent_area(current_state, "RIGHT"))
+        x = 0
+    if y < - transition_threshold:
+        change_gamestate(find_adjacent_area(current_state, "UP"))
+        y = HEIGHT
+    if y > HEIGHT + transition_threshold:
+        change_gamestate(find_adjacent_area(current_state, "DOWN"))
+        y = 0
+        
+
+    # Draw "Player"
+    #pygame.draw.rect(screen, BLUE, (x, y, size, size))
+    fake_player_interaction_rect = pygame.Rect(x - size/2, y - size/2, size + obj_range, size + obj_range)
+
+    screen.blit(player.image, player.rect)
+    player.update()
+
+
+    #   DRAW WORLD OBJECTS 
+    for obj in world_objects:
+        pygame.draw.rect(screen, obj.COLOR, (obj.x, obj.y, obj.size, obj.size))
+    
+    for obj in world_objects:
+        if fake_player_interaction_rect.colliderect(obj.interaction_rect):
+            print("Can interact!")
+            #if player presses space:
+                # Code to handle interactin
+        else:
+            print("Cant interact :3")
+
+    #   DRAW DIALOGUE BOXES:
+    
+
 
     # Update display
     pygame.display.flip()
